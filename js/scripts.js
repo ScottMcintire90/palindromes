c50a5bc0707961d270d0e74bf8127c59f5cc4930
Jeff Dean and Scott McIntire <deanjef2@hotmail.com>
2016-02-29 18:56:27 -0800

    add jquery logic to the user input

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,15 @@
 $(document).ready(function() {
   $("form#palindrome").submit(function(event) {
     var userWord = $("input#userWord").val();
+    var userArray = userWord.split('');
+    var userArrayReverse = userWord.split('').reverse('');
 
-
-  });
-
-
-
+    if (userArray === userArrayReverse) {
+      $(".result").show();
+    }
+    else {
+      $(".result").show(".notPalindrome");
+    }
   event.preventDefault();
+  });
 });
