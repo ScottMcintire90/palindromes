93e7c42bee2f513d699bab847f36782787632d9a
Jeff Dean and Scott McIntire <scottmcintire90@gmail.com>
2016-02-29 18:32:51 -0800

    adds initial jquery, css styling, and basic html text with form and submit button

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+
+
+$(document).ready(function() {
+  $("form#palindrome").submit(function(event) {
+    var userWord = $("input#userWord").val();
+
+
+  });
+
+
+
+  event.preventDefault();
+});
